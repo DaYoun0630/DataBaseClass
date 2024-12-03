40efaeed1d24907dd7d81e2a4dfbd43513a9dbc4
hs-2171042-sonjihyoung <115959290+hs-2171042-sonjihyoung@users.noreply.github.com>
2024-12-03 12:34:08 +0900

    Update mypage.js
    탈퇴할 때 메시지 하나 추가해야하나?

--- Self/js/mypage.js
+++ Self/js/mypage.js
@@ -1,13 +1,15 @@
 
 
-document.querySelector('.apply-btn').addEventListener('click', function() {
-    alert('알림 설정이 적용됐습니다.');
+document.getElementById('userProfile-btn').addEventListener('click', function() {
+    window.location.href = 'userProfile.html'; // 개인정보 페이지로 이동
 });
 
-document.getElementById('editsignup-link').addEventListener('click', function() {
-    window.location.href = 'editsignup.html'; // 개인정보 수정 페이지로 이동
+document.getElementById('notification-btn').addEventListener('click', function() {
+    window.location.href = 'nofication.html'; // 알림페이지로 이동
 });
 
-document.getElementById('mainpage-link').addEventListener('click', function() {
-    window.location.href = 'index.html'; // 메인페이지로 이동
+document.getElementById('delete-account-btn').addEventListener('click', function() {
+    alert('탈퇴가 완료됐습니다.');
+    // 한 번 확인 받는 페이지 필요?
+    window.location.href = 'login.html'; // 다시 로그인 페이지로 이동
 });
