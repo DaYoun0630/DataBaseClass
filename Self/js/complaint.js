1968d29a96dd0b312ef56593a3bd9851251ec0cc
hs-2171042-sonjihyoung <115959290+hs-2171042-sonjihyoung@users.noreply.github.com>
2024-12-03 11:23:50 +0900

    Delete Self directory

--- Self/js/complaint.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('create-complaint-btn').addEventListener('click', function() {
-    window.location.href = 'complaintCreate.html'; // 민원 작성 페이지로 이동
-});
-
-document.getElementById('mainpage-link').addEventListener('click', function() {
-    window.location.href = 'index.html'; // 메인 페이지로 이동
-});
